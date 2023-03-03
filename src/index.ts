d5103743f369d2fb70394539e531f53fe8b5c0ea
Kethlyn <kethlynsaraa@gmail.com>
2023-03-02 23:23:09 -0300

    initial setup

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+import app from "./app/app.js";
+dotenv.config();
+
+const port = +process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server is up and running on port ${port}`);
+});
